957d13c23151c4f7a05a380950d25afeaf9d473a
Mohammad Ahmed <yahmed786222@gmail.com>
2020-08-09 18:04:33 +0530

    Book List Application in ES6 way

---
+++ BookList/appEs6.js
@@ -0,0 +1,93 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+    addBookToList(book) {
+        const list = document.getElementById('book-list');
+
+        //create table row element
+        const row = document.createElement('tr');
+        //Insert columns
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="" class="delete">x</a></td>
+        `;
+        list.appendChild(row);
+    }
+
+    showAlert(message, className) {
+        //create element
+        const div = document.createElement('div');
+        //add className
+        div.className = `alert ${className}`;
+        //add text
+        div.appendChild(document.createTextNode(message));
+        //get parent where to insert
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+
+        container.insertBefore(div, form);
+
+        //set timeout
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
+    deleteBook(target) {
+        if(target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+            const ui = new UI();
+            ui.showAlert('Book Removed Successfully!', 'success');
+        }
+    }
+
+    clearFields() {
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+    }
+}
+
+//Event Listener for add book
+document.getElementById("book-form").addEventListener('submit', function(e) {
+    //get form values
+    const title= document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
+
+    //Instantiate book    
+    const book = new Book(title, author, isbn);
+    //Instantiate UI
+    const ui = new UI();
+
+    //validate the fields
+    if(title === '' ||  author === '' || isbn === '') {
+        // Error alert
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        //Add book to list
+        ui.addBookToList(book);
+        //show success alert
+        ui.showAlert('Book successfully added', 'success');
+
+        //Clear form field
+        ui.clearFields();
+    }
+    
+    e.preventDefault();
+});
+
+//Event Listener for delete ()
+document.getElementById('book-list').addEventListener('click', function(e) {
+    const ui = new UI();
+    ui.deleteBook(e.target);
+    e.preventDefault();
+});
