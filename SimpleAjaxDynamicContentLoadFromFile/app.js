9ceb80e0ab605e05b78d17a1147d5c02afac6947
Mohammad Ahmed <yahmed786222@gmail.com>
2020-08-10 15:34:27 +0530

    Load dynamic content from a json file using AJAX

--- SimpleAjaxDynamicContentLoadFromFile/app.js
+++ SimpleAjaxDynamicContentLoadFromFile/app.js
@@ -6,8 +6,31 @@ function loadData() {
     xhr.onload = function() {
         // Ready status is 4
         if(this.status === 200) {
-            const div = document.getElementById('output');
+            const div = document.getElementById('outputData');
             div.innerHTML = this.responseText;
+        }
+    };
+    //send
+    xhr.send();
+}
+
+function loadInfo() {
+    //create XHR object
+    const xhr = new XMLHttpRequest();
+    //open
+    xhr.open('GET', 'author.json', true); // third parameter set to true for asynchronous
+    xhr.onload = function() {
+        // Ready status is 4
+        if(this.status === 200) {
+            const author = JSON.parse(this.responseText);
+            const div = document.getElementById('outputInfo');
+            div.innerHTML = `
+            <ul>
+                <li>Name: ${author.name}</li>
+                <li>Age: ${author.age}</li>
+                <li>Company: ${author.company}</li>
+                <li>Designation: ${author.designation}</li>
+            </ul>`;
         }
     };
     //send
